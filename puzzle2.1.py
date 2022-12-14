ea1b9a3f0e78ae3c7671aa93b9373c98cafc6fc3
1-Yasmeen <120548939+1-Yasmeen@users.noreply.github.com>
2022-12-14 14:23:04 +0530

    Add files via upload

--- puzzle2.1.py
+++ puzzle2.1.py
@@ -1,2 +1,7 @@
-str=("Twinkle,twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle,twinkle,little star,\n\tHow I wonder what you are")
+str=("Twinkle,twinkle, little star,\n\t"
+     "How I wonder what you are!\n\t\t"
+     "Up above the world so high,\n\t\t"
+     "Like a diamond in the sky.\n"
+     "Twinkle,twinkle,little star,\n\t"
+     "How I wonder what you are")
 print(str)
